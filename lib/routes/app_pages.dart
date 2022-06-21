d461806bb36887730b56783a5e2bcda8afaad436
zahra khoshdel <imzahrakhoshdel@gmail.com>
2022-06-21 23:45:21 +0430

    Add login and sign up routes

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: constant_identifier_names
 
 import 'package:get/get.dart';
+import 'package:task_management/screens/authentication/authentication_bindings.dart';
+import 'package:task_management/screens/authentication/signin_screen.dart';
+import 'package:task_management/screens/authentication/signup_screen.dart';
 import 'package:task_management/screens/home/home_binding.dart';
 import 'package:task_management/screens/home/home_screen.dart';
 import 'package:task_management/screens/tasks/task_screen.dart';
@@ -10,9 +13,19 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.LOGIN;
 
   static final routes = [
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => SignInScreen(),
+      binding: AuthenticationBindings(),
+    ),
+    GetPage(
+      name: _Paths.SIGN_UP,
+      page: () => SignUpScreen(),
+      binding: AuthenticationBindings(),
+    ),
     GetPage(
       name: _Paths.HOME,
       page: () => const HomeScreen(),
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -5,11 +5,15 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
 
+  static const LOGIN = _Paths.LOGIN;
+  static const SIGN_UP = _Paths.SIGN_UP;
   static const HOME = _Paths.HOME;
   static const TASKS = _Paths.TASKS;
 }
 
 abstract class _Paths {
+  static const LOGIN = '/login';
+  static const SIGN_UP = '/sign-up';
   static const HOME = '/';
   static const TASKS = '/tasks';
 }
